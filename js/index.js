50de96340686602e108fadd1a3efd376de4f6785
O009920 <mfuentes@forum.cl>
2024-05-31 10:20:20 -0400

    correcciones de diseño

--- js/index.js
+++ js/index.js
@@ -5,14 +5,14 @@ const users = [
         nombre: "Matias",
         apellido: "Fuentes",
         edad: 28,
-        direccion: "123 Admin St.",
+        direccion: "Los naranjos 0228",
         roles: ["admin"]
     },
     {
-        email: "user1@example.com",
-        password: "user123",
-        nombre: "Matias",
-        apellido: "Fuentes",
+        email: "frojas@gmail.com",
+        password: "admin123",
+        nombre: "Francisca",
+        apellido: "Rojas",
         edad: 28,
         direccion: "456 User Ave.",
         roles: ["customer"]
@@ -32,6 +32,28 @@ function findUser(email, password) {
     return users.find(user => user.email === email && user.password === password);
 }
 
+function showToast(message) {
+    const toast = document.getElementById('login-toast');
+    const toastBody = toast.querySelector('.toast-body');
+    toastBody.childNodes[0].nodeValue = message;  // Update message text
+    const bsToast = new bootstrap.Toast(toast);
+    bsToast.show();
+}
+
+function addAdminMenuOption() {
+    const adminMenuOption = document.getElementById("admin-menu-option");
+    if (adminMenuOption) {
+        adminMenuOption.classList.remove("d-none");
+    }
+}
+
+function removeAdminMenuOption() {
+    const adminMenuOption = document.getElementById("admin-menu-option");
+    if (adminMenuOption) {
+        adminMenuOption.classList.add("d-none");
+    }
+}
+
 function login() {
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
@@ -40,7 +62,7 @@ function login() {
 
     if (user) {
         // Ocultar modal de inicio de sesión
-        document.getElementById("loginModal").style.display = 'none';
+        $('#loginModal').modal('hide');
         document.body.classList.remove('modal-open');
         let backdrops = document.getElementsByClassName('modal-backdrop');
         while (backdrops.length > 0) {
@@ -53,9 +75,15 @@ function login() {
         // Actualizar navbar para mostrar el nombre del usuario y el submenú
         document.getElementById("login-link").classList.add("d-none");
         document.getElementById("user-menu").classList.remove("d-none");
-        document.getElementById("userName").innerText =  user.nombre;
+        document.getElementById("userName").innerText = `Hola, ${user.nombre}`;
+
+        // Agregar opción "Administración" al menú si el usuario es admin
+        if (user.roles.includes("admin")) {
+            addAdminMenuOption();
+        }
     } else {
-        alert("Correo electrónico o contraseña incorrectos.");
+        // Mostrar toast
+        showToast("Correo electrónico o contraseña incorrectos.");
     }
 }
 
@@ -68,11 +96,15 @@ function logout() {
     document.getElementById("user-menu").classList.add("d-none");
     document.getElementById("userName").innerText = '';
 
+    // Limpiar los campos del formulario de inicio de sesión
+    document.getElementById("email").value = '';
+    document.getElementById("password").value = '';
+
+    // Remover la opción "Administración" del menú
+    removeAdminMenuOption();
+
     // Mostrar modal de inicio de sesión
-    document.getElementById("loginModal").style.display = 'block';
-    document.body.classList.add('modal-open');
-    document.getElementById("email").value= '';
-    document.getElementById("password").value= '';
+    $('#loginModal').modal('show');
 }
 
 function checkLoginStatus() {
@@ -82,6 +114,11 @@ function checkLoginStatus() {
         document.getElementById("login-link").classList.add("d-none");
         document.getElementById("user-menu").classList.remove("d-none");
         document.getElementById("userName").innerText = `Hola, ${user.nombre}`;
+
+        // Agregar opción "Administración" al menú si el usuario es admin
+        if (user.roles.includes("admin")) {
+            addAdminMenuOption();
+        }
     }
 }
 
@@ -89,16 +126,6 @@ function checkLoginStatus() {
 document.getElementById("loginForm").addEventListener("submit", function(event) {
     event.preventDefault();
     login();
-});
-
-// Añadir event listener al botón de cierre del modal
-document.querySelector('#loginModal .close').addEventListener('click', function() {
-    document.getElementById("loginModal").style.display = 'none';
-    document.body.classList.remove('modal-open');
-    let backdrops = document.getElementsByClassName('modal-backdrop');
-    while (backdrops.length > 0) {
-        backdrops[0].parentNode.removeChild(backdrops[0]);
-    }
 });
 
 // Verificar el estado de inicio de sesión al cargar la página
